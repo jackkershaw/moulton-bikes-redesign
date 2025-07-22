6f298cba3d4c5feb15eb37d1366099a1b80cf030
jackkershaw <hello@jackkershaw.net>
2025-07-22 19:33:41 +0100

    add video

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,19 @@ export default function HomePage() {
   return (
     <>
       <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
-          Hello! This is the main homepage content.
+        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:gap-0 md:px-4 min-[1320px]:px-0 space-y-10">
+          {/*<video
+            width="2000"
+            height="240"
+            autoPlay
+            preload="auto"
+            loop
+          >
+            <source src="/xtb.mp4" type="video/mp4" />
+            Your browser does not support the video tag. // #TODO: ADD
+            image here.//
+          </video> */}
+          <div>Hello! This is the main homepage content.</div>
         </div>
       </div>
       <Footer />
