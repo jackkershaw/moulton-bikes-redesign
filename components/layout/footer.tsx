3bae869767b2b32c234bb3685a3203198423c562
jackkershaw <hello@jackkershaw.net>
2025-07-22 19:06:00 +0100

    edit footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,19 +1,10 @@
-import { getMenu } from "lib/shopify";
-
 const { COMPANY_NAME, SITE_NAME } = process.env;
 
 export default async function Footer() {
-  const currentYear = new Date().getFullYear();
-  const copyrightDate = 2025;
-  const menu = await getMenu("next-js-frontend-footer-menu");
-  const copyrightName = COMPANY_NAME || SITE_NAME || "";
-
   return (
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
       <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
-          <p>{copyrightDate}</p>
-          <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
           <p>
             <a href="https://github.com/jackkershaw/moulton-bikes-redesign">
               View source on Github
