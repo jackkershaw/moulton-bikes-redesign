131f02952a83f2241b16c3c4adfdfd85525d4997
jackkershaw <hello@jackkershaw.net>
2025-07-22 18:57:28 +0100

    adding images and editing template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
-import { CartProvider } from 'components/cart/cart-context';
-import { Navbar } from 'components/layout/navbar';
-import { WelcomeToast } from 'components/welcome-toast';
-import { GeistSans } from 'geist/font/sans';
-import { getCart } from 'lib/shopify';
-import { ReactNode } from 'react';
-import { Toaster } from 'sonner';
-import './globals.css';
-import { baseUrl } from 'lib/utils';
+import { CartProvider } from "components/cart/cart-context";
+import { Navbar } from "components/layout/navbar";
+import { GeistSans } from "geist/font/sans";
+import { getCart } from "lib/shopify";
+import { baseUrl } from "lib/utils";
+import { ReactNode } from "react";
+import { Toaster } from "sonner";
+import "./globals.css";
 
 const { SITE_NAME } = process.env;
 
@@ -14,16 +13,16 @@ export const metadata = {
   metadataBase: new URL(baseUrl),
   title: {
     default: SITE_NAME!,
-    template: `%s | ${SITE_NAME}`
+    template: `%s | ${SITE_NAME}`,
   },
   robots: {
     follow: true,
-    index: true
-  }
+    index: true,
+  },
 };
 
 export default async function RootLayout({
-  children
+  children,
 }: {
   children: ReactNode;
 }) {
@@ -38,7 +37,6 @@ export default async function RootLayout({
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
           </main>
         </CartProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Carousel } from "components/carousel";
-import { ThreeItemGrid } from "components/grid/three-items";
 import Footer from "components/layout/footer";
 
 export const metadata = {
@@ -13,9 +11,11 @@ export const metadata = {
 export default function HomePage() {
   return (
     <>
-      <ThreeItemGrid />
-      Hello
-      <Carousel />
+      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
+        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
+          Hello! This is the main homepage content.
+        </div>
+      </div>
       <Footer />
     </>
   );
